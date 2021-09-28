b392b555b3cf387737d6dd293cb31a3f2b5b3759
Sarah Fuoss <sfuoss@gmail.com>
2021-09-28 14:20:23 -0500

    Completed questions 3,4,6

--- joins-solution.sql
+++ joins-solution.sql
@@ -6,3 +6,24 @@ JOIN "addresses" ON "addresses"."customer_id" = "customers"."id";
 SELECT "orders"."order_date", "products"."description", "line_items"."order_id", "line_items"."quantity"  FROM "products"
 JOIN "line_items" ON "products"."id"="line_items"."product_id" 
 JOIN "orders" ON "orders"."id"="line_items"."order_id";
+
+-- 3. Which warehouses have cheetos?
+SELECT "warehouse"."warehouse" FROM "warehouse"
+JOIN "warehouse_product" on "warehouse_product"."warehouse_id"="warehouse"."id"
+JOIN "products" ON "products"."id"="warehouse_product"."product_id"
+WHERE "products"."description" = 'cheetos';
+
+-- 4. Which warehouses have diet pepsi?
+SELECT "warehouse"."warehouse" FROM "warehouse"
+JOIN "warehouse_product" on "warehouse_product"."warehouse_id"="warehouse"."id"
+JOIN "products" ON "products"."id"="warehouse_product"."product_id"
+WHERE "products"."description" = 'diet pepsi';
+
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results. (This lists the order count but not name)
+SELECT count("orders"."id") FROM "orders"
+JOIN "addresses" on "orders"."address_id"="addresses"."id"
+JOIN "customers" ON "customers"."id"="addresses"."customer_id" GROUP BY "customer_id"; 
+
+
+--6. How many customers do we have?
+SELECT count(*) from "customers";
